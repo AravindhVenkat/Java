1b304e5cabc778fab1a84ca158ed85dce9d6f815
Aravindh V <90633974+AravindhVenkat@users.noreply.github.com>
2021-11-08 00:15:12 +0530

    Update guessinggame.java

--- guessinggame.java
+++ guessinggame.java
@@ -5,24 +5,25 @@ public class Main {
         int generatedNumber = (int) (Math.random()*100 + 1);
         int userGuessedNumber = 0;
         System.out.println("The correct guess would be " + generatedNumber);
-        int count = 1;
+        int count = 0;
+        int score = 100;
 
         while (userGuessedNumber != generatedNumber)
         {
             String response = JOptionPane.showInputDialog(null,
                     "Enter a guess between 1 and 100", "Guessing Game", JOptionPane.QUESTION_MESSAGE);
             userGuessedNumber = Integer.parseInt(response);
-            JOptionPane.showMessageDialog(null, ""+ determineGuess(userGuessedNumber, generatedNumber, count));
+            JOptionPane.showMessageDialog(null, ""+ determineGuess(userGuessedNumber, generatedNumber, count, score));
             count++;
         }
     }
 
-    public static String determineGuess(int userGuessedNumber, int generatedNumber, int count){
+    public static String determineGuess(int userGuessedNumber, int generatedNumber, int count, int score){
         if (userGuessedNumber <=0 || userGuessedNumber >100) {
             return "Your guess is invalid";
         }
         else if (userGuessedNumber == generatedNumber){
-            return "Correct!\nTotal Guesses: " + count;
+            return "Correct!\nTotal Guesses: " + (count+1) + " and your score is " + (score - (count * 10));
         }
         else if (userGuessedNumber > generatedNumber) {
             return "Your guess is greater, try a lesser number.\nTry Number: " + (userGuessedNumber - 1);
